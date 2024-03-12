c30db36c820728f8c3f306600cbb628844107e5a
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-12 11:50:34 +0530

    feat: learn routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/home";
+import { Service } from "./components/service";
+import { About } from "./components/about";
+import { Contact } from "./components/contact";
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/service" element={<Service/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
