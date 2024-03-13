ac3024863a448cb24995b67874a3cb87d029b37a
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-13 13:54:06 +0530

    feat: complete total conversion from html css to react

--- src/components/about.js
+++ src/components/about.js
@@ -8,14 +8,14 @@ export const About = () => {
       <header>
         <h1>About Us</h1>
     </header>
-    <div className="item1">
-        <div className="item3">
-            <h1 className = "head">How it started</h1>
-            <h1 className = "subhead">A dead end is just a good place<br/>to turn around</h1>
-            <h5 className = "description">Welcome to ReadRover, where readers embark on literary journeys and authors find their voice. Explore a world of endless stories, connect with fellow book lovers, and discover the next chapter of your reading adventure. Whether you're seeking captivating tales or eager to share your own, join our community and let the pages of imagination unfold.</h5>
+    <div className="aboutitem1">
+        <div className="aboutitem3">
+            <h1 className="abouthead">How it started</h1>
+            <h1 className="aboutsubhead">A dead end is just a good place<br/>to turn around</h1>
+            <h5 className="aboutdescription">Welcome to ReadRover, where readers embark on literary journeys and authors find their voice. Explore a world of endless stories, connect with fellow book lovers, and discover the next chapter of your reading adventure. Whether you're seeking captivating tales or eager to share your own, join our community and let the pages of imagination unfold.</h5>
         </div> 
         
-        <div className="item2">
+        <div className="aboutitem2">
             <img src={aboutImage}/>
         </div>  
     </div>
--- src/components/contact.js
+++ src/components/contact.js
@@ -2,6 +2,39 @@ import React from 'react'
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className="main">
+        <div className="image">
+            <img src="book3.png" alt="img" width="60%" height="30%"/>
+            <div className="label">
+            <h1>Contact Us</h1>
+                <input type="text" id="firstname" name="ftname" placeholder= "Enter your name :" size="20" required/>   <br/><br/>
+                <input type="text" id="email" name="Email" placeholder= "Enter a valid email address :" size="20" required/> <br/><br/>
+                <input type="text" id="phone" name="Phone" placeholder="Phone Number" size="10" required/> <br/><br/>
+                Message : <br/>
+                <textarea cols="80" rows="7" value="Message"></textarea> 
+                <button onclick="SubmitEvent">SEND MESSAGE</button>
+            </div>
+        </div>
+        <div className="icon">
+            <div className="map">
+                <img src="placeholder.png"/>
+                <p>Our&nbsp;Main&nbsp;Location</p>
+            </div>
+            <div className="phone">
+                <img src="telephone.png"/>
+                <p>Phone&nbsp;Number</p>
+            </div>
+            <div className="mail">
+                <img src="mail.png"/>
+                <p>E-&nbsp;Mail</p>
+            </div>
+            <div className="time">
+                <img src="alarm.png"/>
+                <p>Opening&nbsp;Time</p>
+            </div>
+        </div>
+    </div>
+    </div>
   )
 }
